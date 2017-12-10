36f767c9d4363430d2e4852000a2944f008d8c4c
Lyubov Mac <lyubov@voloshko.com>
2017-12-10 15:53:06 +0200

    add webpack

---
+++ src/main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by macbook11 on 10/12/17.
+ */
+
+console.log('Hello World!');
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/**
+ * Created by macbook11 on 10/12/17.
+ */
+
+const path = require('path');
+
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    entry: [
+        './main.js',
+    ],
+    output: {
+        path: path.join(__dirname, 'www'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        modules: [
+            path.join(__dirname, 'node_modules'),
+        ],
+    },
+};
